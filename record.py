e45b923e8687ec21f7e3a8fab5d6397ff607a38d
ensta-cesar <ensta-cesar@mesogip.r2.enst.fr>
2025-03-05 16:10:38 +0100

    update recording

--- record.py
+++ record.py
@@ -43,5 +43,5 @@ def save_video(env, agent, video_dir_path='videos', max_steps=3000):
     # Save the frames to MP4 using imageio
     video_path = os.path.join(video_dir_path, f"final_run_{int(time.time())}.mp4")
     # codec='libx264' lets you control compression, etc.
-    imageio.mimsave(video_path, frames, fps=10, macro_block_size=None)
+    imageio.mimsave(video_path, frames, fps=25, macro_block_size=None)
     return video_path
